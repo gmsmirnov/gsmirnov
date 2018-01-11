74784c967558546656e5f5a5e5aadfaf71e2d587
gmsmirnov <artress@ngs.ru>
2018-01-12 02:16:50 +0700

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,19 @@
 package ru.job4j;
 
+/**
+* Calculate - простой класс, содежащий метод main()
+* @autor gsmirnov
+* @since 12.01.2018
+* @version 1.0
+*/
+
 public class Calculate {
+	
+	/**
+	* Метод main() - выводит сообщение "Hello, world!"
+	*/
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
 	}
+	
 }
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Gregory Smirnov (artress@ngs.ru)
+* @version 1.0
+* @since 12.01.2018
+*/
+package ru.job4j;
