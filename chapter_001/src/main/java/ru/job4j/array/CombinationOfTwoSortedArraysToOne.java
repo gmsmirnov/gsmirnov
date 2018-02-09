475800daf42a3f2628858263fc57aee6cc4a123a
gmsmirnov <artress@ngs.ru>
2018-02-09 16:39:13 +0700

    Часть 001. Тестовое задание. Копирование из двух отсортированный массивов в один.

--- chapter_001/src/main/java/ru/job4j/array/CombinationOfTwoSortedArraysToOne.java
+++ chapter_001/src/main/java/ru/job4j/array/CombinationOfTwoSortedArraysToOne.java
@@ -1,10 +1,12 @@
 package ru.job4j.array;
 
+import java.util.Arrays;
+
 /**
  * This class combines two sorted arrays in one sorted array.
  *
  * @author Gregory Smirnov (artress@ngs.ru)
- * @version 1.0
+ * @version 1.2
  * @since 02/02/2018
  */
 public class CombinationOfTwoSortedArraysToOne {
@@ -74,6 +76,36 @@ public class CombinationOfTwoSortedArraysToOne {
                 newArray[k] = firstArray[i];
                 i++;
             }
+        }
+        return newArray;
+    }
+
+    /**
+     * Combines two sorted arrays in one.
+     * @param firstArray - the first sorted array.
+     * @param secondArray - the second sorted array.
+     * @throws IllegalArgumentException when one of params is 0-length.
+     * @return new sorted array.
+     */
+    public int[] arraysCombination3(int[] firstArray, int[] secondArray) {
+        if ((firstArray == null) || (secondArray == null)) {
+            throw new IllegalArgumentException("One of arrays is 0-length.");
+        }
+        int[] newArray = new int[firstArray.length + secondArray.length];
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        while ((i < firstArray.length) && (j < secondArray.length)) {
+            if (firstArray[i] <= secondArray[j]) {
+                newArray[k++] = firstArray[i++];
+            } else if (firstArray[i] > secondArray[j]) {
+                newArray[k++] = secondArray[j++];
+            }
+        }
+        if (i == firstArray.length) {
+            System.arraycopy(secondArray, j, newArray, k, secondArray.length - j);
+        } else if (j == secondArray.length) {
+            System.arraycopy(firstArray, i, newArray, k, firstArray.length - i);
         }
         return newArray;
     }
--- chapter_001/src/test/java/ru/job4j/array/CombinationOfTwoSortedArraysToOneTest.java
+++ chapter_001/src/test/java/ru/job4j/array/CombinationOfTwoSortedArraysToOneTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.assertThat;
  * Testing 'CombinationOfTwoSortedArraysToOne'.
  * @author Gregory Smirnov (artress@ngs.ru)
  * @since 02/02/2018
- * @version 1.0
+ * @version 1.2
  */
 public class CombinationOfTwoSortedArraysToOneTest {
     @Test
@@ -27,6 +27,16 @@ public class CombinationOfTwoSortedArraysToOneTest {
         int[] firstArray = {4, 6, 8, 10};
         int[] secondArray = {1, 2, 3, 4, 5, 6, 7, 8};
         int[] result = combo.arraysCombination2(firstArray, secondArray);
+        int[] expected = {1, 2, 3, 4, 4, 5, 6, 6, 7, 8, 8, 10};
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenTwoSortedArraysThenOneSortedArray3() {
+        CombinationOfTwoSortedArraysToOne combo = new CombinationOfTwoSortedArraysToOne();
+        int[] firstArray = {4, 6, 8, 10};
+        int[] secondArray = {1, 2, 3, 4, 5, 6, 7, 8};
+        int[] result = combo.arraysCombination3(firstArray, secondArray);
         int[] expected = {1, 2, 3, 4, 4, 5, 6, 6, 7, 8, 8, 10};
         assertThat(result, is(expected));
     }
