9ffd2829ce2099380811af20540e413262aada38
gmsmirnov <artress@ngs.ru>
2018-02-18 01:05:08 +0700

    4. Используя шаблон проектирования - стратегию [#786]

--- chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
@@ -10,28 +10,37 @@ import static org.junit.Assert.assertThat;
  * Tests Paint.
  *
  * @author Gregory Smirnov (artress@ngs.ru)
- * @version 1.0
+ * @version 1.1
  * @since 17/02/2018
  */
 public class PaintTest {
     @Test
     public void whenDrawSquare() {
-        // получаем ссылку на стандартный вывод в консоль.
         PrintStream stdout = System.out;
-        // Создаем буфур для хранения вывода.
         ByteArrayOutputStream out = new ByteArrayOutputStream();
-        //Заменяем стандартный вывод на вывод в пямять для тестирования.
         System.setOut(new PrintStream(out));
-        // выполняем действия пишушиее в консоль.
         new Paint().draw(new Square(4));
-        // проверяем результат вычисления
         assertThat(new String(out.toByteArray()), is(new StringBuilder()
                 .append("++++").append(System.lineSeparator())
                 .append("++++").append(System.lineSeparator())
                 .append("++++").append(System.lineSeparator())
                 .append("++++").append(System.lineSeparator())
                 .toString()));
-        // возвращаем обратно стандартный вывод в консоль.
+        System.setOut(stdout);
+    }
+
+    @Test
+    public void whenDrawTriangle() {
+        PrintStream stdout = System.out;
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        new Paint().draw(new Triangle(4));
+        assertThat(new String(out.toByteArray()), is(new StringBuilder()
+                .append("   +   ").append(System.lineSeparator())
+                .append("  +++  ").append(System.lineSeparator())
+                .append(" +++++ ").append(System.lineSeparator())
+                .append("+++++++").append(System.lineSeparator())
+                .toString()));
         System.setOut(stdout);
     }
 }
