28b1150f220429c7a9fbefeda444a1a4a83de57e
gmsmirnov <artress@ngs.ru>
2018-09-14 09:08:00 +0700

    2. Обеспечить остановку потребителя. [#66825]

--- chapter_2-002/src/main/java/ru/job4j/blckqueue/ParallelSearch.java
+++ chapter_2-002/src/main/java/ru/job4j/blckqueue/ParallelSearch.java
@@ -1,28 +1,23 @@
 package ru.job4j.blckqueue;
 
-import net.jcip.annotations.GuardedBy;
-import net.jcip.annotations.ThreadSafe;
-
 /**
  * Parallel Search.
  *
  * @author Gregory Smirnov (artress@ngs.ru)
- * @version 1.0
+ * @version 1.1
  * @since 25/08/2018
  */
 public class ParallelSearch {
     public static void main(String[] args) {
-        int size = 10;
+        int size = 2;
         SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<Integer>(size);
         final Thread consumer = new Thread(() -> {
-            while (true) {
+            while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
                 try {
                     System.out.println(queue.poll());
                 } catch (InterruptedException ie) {
                     ie.printStackTrace();
                     Thread.currentThread().interrupt();
-                    System.out.println("Consumer interrupted.");
-                    break;
                 }
             }
         });
