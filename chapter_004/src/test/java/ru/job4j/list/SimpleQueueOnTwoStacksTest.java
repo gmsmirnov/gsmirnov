b1fdd758cfa7657417c0f62b20aaa8d17c9d2e9d
gmsmirnov <artress@ngs.ru>
2018-09-17 20:24:39 +0700

    5.3.3.1 Очередь на двух стеках [#160]

--- chapter_004/src/test/java/ru/job4j/list/SimpleQueueOnTwoStacksTest.java
+++ chapter_004/src/test/java/ru/job4j/list/SimpleQueueOnTwoStacksTest.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.*;
  * Simple queue based on two stacks with generic type test.
  *
  * @author Gregory Smirnov (artress@ngs.ru)
- * @version 1.0
+ * @version 1.1
  * @since 16/09/2018
  */
 public class SimpleQueueOnTwoStacksTest {
@@ -17,43 +17,46 @@ public class SimpleQueueOnTwoStacksTest {
 
     @Test
     public void whenPushInDirectOrderThenPollInDirectOrder() {
-        int size = 10;
-        for (int i = 0; i < size; i++) {
-            this.queue.push(i);
-        }
-        for (int i = 0; i < size; i++) {
-            assertThat(this.queue.poll(), is(i));
-        }
-    }
-
-    @Test (expected = IndexOutOfBoundsException.class)
-    public void whenPushAndPollInDirectOrderAlternateThenPollInDirectOrder() {
-        for (int i = 0; i < 10; i++) {
-            this.queue.push(i);
-        }
+        this.queue.push(0);
+        this.queue.push(1);
+        this.queue.push(2);
         assertThat(this.queue.poll(), is(0));
         assertThat(this.queue.poll(), is(1));
         assertThat(this.queue.poll(), is(2));
-        assertThat(this.queue.poll(), is(3));
-        assertThat(this.queue.poll(), is(4));
-        assertThat(this.queue.poll(), is(5));
-        for (int i = 10; i < 20; i++) {
-            this.queue.push(i);
-        }
-        assertThat(this.queue.poll(), is(6));
-        assertThat(this.queue.poll(), is(7));
-        assertThat(this.queue.poll(), is(8));
-        assertThat(this.queue.poll(), is(9));
-        assertThat(this.queue.poll(), is(10));
-        assertThat(this.queue.poll(), is(11));
-        assertThat(this.queue.poll(), is(12));
-        assertThat(this.queue.poll(), is(13));
-        assertThat(this.queue.poll(), is(14));
-        assertThat(this.queue.poll(), is(15));
-        assertThat(this.queue.poll(), is(16));
-        assertThat(this.queue.poll(), is(17));
-        assertThat(this.queue.poll(), is(18));
-        assertThat(this.queue.poll(), is(19));
-        assertThat(this.queue.poll(), is(20));
+    }
+
+    @Test
+    public void whenPushAndPollInDirectOrderAlternate1() {
+        this.queue.push(0);
+        assertThat(this.queue.poll(), is(0));
+        this.queue.push(1);
+        this.queue.push(2);
+        assertThat(this.queue.poll(), is(1));
+        assertThat(this.queue.poll(), is(2));
+    }
+
+    @Test
+    public void whenPushAndPollInDirectOrderAlternate2() {
+        this.queue.push(0);
+        this.queue.push(1);
+        assertThat(this.queue.poll(), is(0));
+        this.queue.push(2);
+        assertThat(this.queue.poll(), is(1));
+        assertThat(this.queue.poll(), is(2));
+    }
+
+    @Test
+    public void whenPushAndPollInDirectOrderAlternate3() {
+        this.queue.push(0);
+        this.queue.push(1);
+        assertThat(this.queue.poll(), is(0));
+        assertThat(this.queue.poll(), is(1));
+        this.queue.push(2);
+        assertThat(this.queue.poll(), is(2));
+    }
+
+    @Test (expected = IndexOutOfBoundsException.class)
+    public void whenPollFromEmptyQueue() {
+        assertThat(this.queue.poll(), is(0));
     }
 }
