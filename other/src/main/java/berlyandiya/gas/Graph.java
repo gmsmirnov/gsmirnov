b74c91547e2abb72df1ec8ed1b488bb1aa77b09a
gmsmirnov <artress@ngs.ru>
2019-05-16 16:26:04 +0700

    Олимпиада [#135909]

--- other/src/main/java/berlyandiya/gas/Graph.java
+++ other/src/main/java/berlyandiya/gas/Graph.java
@@ -1,4 +1,38 @@
 package berlyandiya.gas;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Graph {
+    private List<Node> graph = new LinkedList<Node>();
+
+    static class Node {
+        private int value;
+
+        List<Integer> weights = new LinkedList<Integer>();
+
+        public Node(int value, List<Integer> weights) {
+            this.value = value;
+            this.weights = weights;
+        }
+
+        public int getValue() {
+            return this.value;
+        }
+
+        public List<Integer> getWeights() {
+            return this.weights;
+        }
+
+        /**
+         * Deletes unnecessary weights.
+         */
+        public void modifyNode(List<Integer> weights) {
+            for (Integer weight : weights) {
+                if (this.weights.contains(weight)) {
+                    this.weights.remove(weight);
+                }
+            }
+        }
+    }
 }
